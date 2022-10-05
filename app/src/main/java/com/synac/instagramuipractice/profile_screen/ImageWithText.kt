a42415195c2351b407e75c895cdf68e4a1c2190a
rahul <rs2570089@gmail.com>
2022-10-06 00:22:50 +0530

    Profile screen and reels UI added

---
+++ app/src/main/java/com/synac/instagramuipractice/profile_screen/ImageWithText.kt
@@ -0,0 +1,8 @@
+package com.synac.instagramuipractice.profile_screen
+
+import androidx.compose.ui.graphics.painter.Painter
+
+data class ImageWithText(
+    val image: Painter,
+    val text: String
+)
